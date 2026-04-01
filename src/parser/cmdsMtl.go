e10b0d21494017c6a6cfc426a37cc99ea24d7fe7
HoelK <KeromnesHoel@gmail.com>
2026-04-01 14:48:56 +0200

    optimized and improved readability of cmdsMtl

--- src/parser/cmdsMtl.go
+++ src/parser/cmdsMtl.go
@@ -3,88 +3,56 @@ package parser
 import "errors"
 import "strconv"
 
+func checkArgs(args []string, amount int) error {
+	if len(args) > amount { return errors.New("[" + args[0] + "] Too much arguments (" + string(amount) + " needed)") }
+	if len(args) < amount { return errors.New("[" + args[0] + "] Missing arguments (" + string(amount) + " needed)") }
+	return nil
+}
+
+func parseRGB(name string, args []string, rgb *[3]float64) (error) {
+	var err error
+
+	if err := checkArgs(args, 4); err != nil { return err }
+	for i := 0; i < 3; i++ {
+		if rgb[i], err = strconv.ParseFloat(args[1], 64); err != nil {
+			return errors.New("[" + name + "] Failed Convertion [\"" + args[i + 1] + "\"] to [float64]")
+		}
+	}
+	return nil
+}
+
+func parseFloat(name string, args []string, num *float64) error {
+	var err error
+
+	if err := checkArgs(args, 2); err != nil { return err }
+	if *num, err = strconv.ParseFloat(args[1], 64); err != nil {
+		return errors.New("[" + name + "] Failed Convertion [\"" + args[1] + "\"] to [float64]")
+	}
+	return nil
+}
+
 func newmtl(data *Data, args []string) (*Material, error) {
 	var mtl Material
-	
-	if (len(args) < 2) { return nil, errors.New("[newmtl] Missing argument (mtl name needed)") }
-	if (len(args) > 2) { return nil, errors.New("[newmtl] Too much arguments") }
+
+	if err := checkArgs(args, 2); err != nil { return nil, err }
 
 	mtl.Name = args[1]
 	data.Mtls = append(data.Mtls, mtl)
 	return &data.Mtls[len(data.Mtls) - 1], nil
 }
 
-func Ns(mtl *Material, args []string) error {
-	var err error
 
-	if (len(args) < 2) { return errors.New("[Ns] Missing argument") }
-	if (len(args) > 2) { return errors.New("[Ns] Too much arguments") }
-
-	if mtl.Ns, err = strconv.ParseFloat(args[1], 64); err != nil { return errors.New("[Ns] Failed Convertion [\"" + args[1] + "\"] to [float64]") }
-	return nil
-}
-
-func Ka(mtl *Material, args []string) error {
-	var err error
-
-	if (len(args) < 4) { return errors.New("[Ka] Missing argument") }
-	if (len(args) > 4) { return errors.New("[Ka] Too much arguments") }
-
-	for i := 0; i < 3; i++ {
-		if mtl.Ka[i], err = strconv.ParseFloat(args[1], 64); err != nil { return errors.New("[Ka] Failed Convertion [\"" + args[1] + "\"] to [float64]") }
-	}
-	return nil
-}
-func Kd(mtl *Material, args []string) error {
-	var err error
-
-	if (len(args) < 4) { return errors.New("[Kd] Missing argument") }
-	if (len(args) > 4) { return errors.New("[Kd] Too much arguments") }
-
-	for i := 0; i < 3; i++ {
-		if mtl.Kd[i], err = strconv.ParseFloat(args[1], 64); err != nil { return errors.New("[Kd] Failed Convertion [\"" + args[1] + "\"] to [float64]") }
-	}
-	return nil
-}
-
-func Ks(mtl *Material, args []string) error {
-	var err error
-
-	if (len(args) < 4) { return errors.New("[Ks] Missing argument") }
-	if (len(args) > 4) { return errors.New("[Ks] Too much arguments") }
-
-	for i := 0; i < 3; i++ {
-		if mtl.Ks[i], err = strconv.ParseFloat(args[1], 64); err != nil { return errors.New("[Ks] Failed Convertion [\"" + args[1] + "\"] to [float64]") }
-	}
-	return nil
-	
-}
-
-func Ni(mtl *Material, args []string) error {
-	var err error
-
-	if (len(args) < 2) { return errors.New("[Ni] Missing argument") }
-	if (len(args) > 2) { return errors.New("[Ni] Too much arguments") }
-
-	if mtl.Ni, err = strconv.ParseFloat(args[1], 64); err != nil { return errors.New("[Ni] Failed Convertion [\"" + args[1] + "\"] to [float64]") }
-	return nil
-}
-
-func d(mtl *Material, args []string) error {
-	var err error
-
-	if (len(args) < 2) { return errors.New("[d] Missing argument") }
-	if (len(args) > 2) { return errors.New("[d] Too much arguments") }
-
-	if mtl.D, err = strconv.ParseFloat(args[1], 64); err != nil { return errors.New("[d] Failed Convertion [\"" + args[1] + "\"] to [float64]") }
-	return nil
-}
+func Ns(mtl *Material, args []string) error { return parseFloat(args[0], args, &mtl.Ns) }
+func Ni(mtl *Material, args []string) error { return parseFloat(args[0], args, &mtl.Ni) }
+func d(mtl *Material, args []string) error { return parseFloat(args[0], args, &mtl.D) }
+func Ka(mtl *Material, args []string) error { return parseRGB(args[0], args, &mtl.Ka) }
+func Kd(mtl *Material, args []string) error { return parseRGB(args[0], args, &mtl.Kd) }
+func Ks(mtl *Material, args []string) error { return parseRGB(args[0], args, &mtl.Ks) }
 
 func illum(mtl *Material, args []string) error {
 	var err error
 
-	if (len(args) < 2) { return errors.New("[illum] Missing argument") }
-	if (len(args) > 2) { return errors.New("[illum] Too much arguments") }
+	if err := checkArgs(args, 2); err != nil { return err }
 
 	if mtl.Illum, err = strconv.ParseInt(args[1], 10, 64); err != nil { return errors.New("[illum] Failed Convertion [\"" + args[1] + "\"] to [int64]") }
 	return nil
